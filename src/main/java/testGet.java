0ce7fae2219414f0cf3354e8f018020165b466e7
WZPluckeyboy <1403752788@qq.com>
2019-12-29 22:59:42 +0800

    文件

---
+++ src/main/java/testGet.java
@@ -0,0 +1,13 @@
+public class testGet{
+    public static void main(String[] args) {
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+        System.out.println("this is my first demo");
+
+    }
+}
